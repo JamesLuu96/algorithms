21ee9816e24a3316b3d199e0f782565f5ef7673a
Ethan Tong <ethantong.studio@gmail.com>
2021-08-14 17:28:26 -0500

    zzzz

--- interviewAlgos/__tests__/shuffledString.test.js
+++ interviewAlgos/__tests__/shuffledString.test.js
@@ -5,13 +5,13 @@ const testCases = [
     ['', ''],
     ['ozonurnroefneneeio', '01149'],
     ['ohusineoxneiieernogntf', '114689'],
-    ['niihsennnfveehittoweiesiereurieogeienthsttieefnhereneihitgoxevnnennnnxfengnrifituheoiiiogeronnvnennzveeseie', '01123344556677888899999999'],
+    ['ixiiiinirrhneeeeehefneenvnfgnineeonsineniifvennefhnouegxhiztvvstteeteeieirtssineinrneenhngrinoeetwtnoohgeou', '01123344556677888899999999'],
     ['eetrenregvtonnonnostzneeneehgeeinrozieonozheeeigineoininnoheeooen','00011111117888999'],
     ['zenoreo','01'],
     ['eisvnnenieenn','799'],
     ['xeieoiesutgeowiitveesefetnhsfrxiniiehinnunheonnrinhrwinoegsonetooriueenfiitgxnetixnfeeitoerxfgeihitnivtgheneehssnswevvt', '112223344455666667788888999999'],
     ['exigeenihnvfsinhtghevitiveseiingtigveesnefxieheenehnisetxegsnigeitgeehviniiitfntnxniishixesi','5556666677888888899999'],
-
+    ['rzfeneenzgogtueoeowunrigwoenieoorfioteitoereeonewfefvtrorroieehrftfvirwiriegotnrhotnzrzooeeoerorfinvoghooeozeiinoouznezteeeeofzreerezhorozonhifeoewtounfvvzenriooifnznutzeounere', '0000000000000111111112222244444455555888889999']
 ]
 
 testCases.forEach((x, i)=>{
@@ -19,4 +19,5 @@ testCases.forEach((x, i)=>{
         expect(shuffledString(x[0])).toBe(x[1])
     })
 })
+
 
